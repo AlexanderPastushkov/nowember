66c25c2eafabe21e520a13e6d87bbf5278a8ff75
Alexander <alexanderpastfast@gmail.com>
2022-11-14 13:15:43 +0300

    js

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,32 @@
-// let kit = 87;
-// console.log(kit);
-function isPalindrome(x) {
-    // your code here
-    
 
-  }
+
+
+ 
+
+const quest = document.querySelectorAll('.content__question');
+const answer = document.querySelectorAll('.content__answer');
+console.log(answer);
+quest.forEach(item=>{
+  item.addEventListener("click", function(e){
+    answer.forEach(item=> {
+      item.classList.toggle('content__answer-active');
+    });  
+  });
+});
+
+
+// const quest = document.querySelectorAll('.content__question');
+// const answer = document.querySelectorAll('.content__answer');
+// console.log(answer);
+// quest.forEach(item=>{
+//   item.addEventListener("click", function(e){
+//     answer.forEach(function(item) {
+//       item.classList.toggle('content__answer-active');
+//     });  
+//   });
+// });
+
+
+
+
+
