38c0a2a63243c249ce569974b20a701575f54400
Alexander <alexanderpastfast@gmail.com>
2022-11-17 22:39:53 +0300

    ghg

--- src/js/script.js
+++ src/js/script.js
@@ -8,6 +8,39 @@ function documentActions(e) {
   
 }
 //========================================================================================================================================================
+// function addData(){
+//   const email = document.getElementById('formEmail');
+//   const userName = document.getElementById('formName');
+//   localStorage.setItem(userName.name,userName.value);
+//   localStorage.setItem(email.name,email.value);
+
+
+// }
+// function checkData(){
+//   const email = document.getElementById('formEmail').value;
+//   const userName = document.getElementById('formName').value;
+
+
+//    const getName = localStorage.getItem(userName.value);
+//    const getEmail = localStorage.getItem(email.value);
+
+//    if(email==getEmail){
+//     if (userName== getName){
+//       alert("Login Successful!!!");
+//     }
+//     else{
+//       alert("Error name");
+//     }
+
+//    }
+//    else {
+//     alert("Total Error");
+//    }
+// }
+
+
+
+//========================================================================================================================================================
 
 const formFields = document.querySelectorAll('[type="text"]');
 const submitBtn = document.querySelector('[type="submit"]')
@@ -16,22 +49,15 @@ console.log(submitBtn);
  
 submitBtn.addEventListener('click',clearLocalStorage);
 
-
-
-
 function clearLocalStorage() {
   localStorage.clear();
 }
-
 //========================================================================================================================================================
-
 function changeHandler(){
   console.log(this.name, this.value);
   localStorage.setItem(this.name, this.value);
-  
-}
+  }
 //========================================================================================================================================================
-
 function checkStorage() {
   for (let i = 0; i < formFields.length; i++) {
     formFields[i] = localStorage.getItem(formFields[i].name)
@@ -48,15 +74,7 @@ function attachEvents(){
 checkStorage();
 console.log(checkStorage());
 //========================================================================================================================================================
-// function validateForm() {
-//   for (let i = 0; i < formFields.length; i++);
-//    if (formFields == ""){
-//     alert("Error!");
-//     return false;
 
-//    }
-  
-// }
 function validateForm() {
   let x = document.forms["myForm"]["name"].value;
   let y = document.forms["myForm"]["email"].value;
@@ -68,121 +86,5 @@ function validateForm() {
   
 }
 
-
-
-// const blockContent = document.querySelector('.block1-help__content');
-// console.log(blockContent);
-
-// document.addEventListener("click",answer);
-
-// function answer(e) {
-//   if(e.target.closest('.content__question')){
-//     console.log(e.target);
-  
-//     blockContent.classList.toggle('content__answer-active');
-//   }
-//   if(!e.target.closest('.block1-help__content')){
-//     blockContent.classList.remove('content__answer-active');
-//   }}
-
-// const quest = document.querySelectorAll('.content__question');
-// const answer = document.querySelectorAll('.content__answer');
-// console.log(answer);
-// quest.forEach(item=>{
-//   item.addEventListener("click", function(e){
-//     item.querySelector('.content__answer');
-//       item.classList.toggle('content__answer-active');
-//     });  
-//   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- // Создаю открывающееся/закрывающееся меню (закрываться будет и при нажатии на Escape)
-// const menuBody = document.querySelector('.menu')
-// function menu(e) {
-// 	if (e.target.closest('.menu__button')) {
-// 		menuBody.classList.toggle('_active')
-// 	}
-// 	if (!e.target.closest('.menu')) {
-// 		menuBody.classList.remove('_active')
-// 	}
-// }
-// document.addEventListener('click', menu)
-// document.addEventListener('keyup', function (e) {
-// 	if (e.code === 'Escape') {
-// 		menuBody.classList.remove('_active')
-// 	}
-// })
-
-// const quest = document.querySelectorAll('.content__question');
-// const answer = document.querySelectorAll('.content__answer');
-// console.log(answer);
-// quest.forEach(item=>{
-//   item.addEventListener("click", function(e){
-//     item.querySelector('.content__answer')
-//       item.classList.toggle('content__answer-active');
-//     });  
-//   });
-
-
-
-
-
-
-
-
-
-// item.querySelector('.content__answer');
-
-
-// const block = document.querySelectorAll('.block1-help__content');
-// console.log(block);
-// quest.forEach(item=>{
-// item.addEventListener("click",function returnAnswer (event){
-//   console.log(event.target);
-// });
-
-
-
-
-
-
-
-// const quest = document.querySelectorAll('.content__question');
-// const answer = document.querySelectorAll('.content__answer');
-// console.log(answer);
-// quest.forEach(item=>{
-//   item.addEventListener("click", function(e){
-//     answer.forEach(function(item) {
-
-//       item.classList.toggle('content__answer-active');
-//     });  
-//   });
-// });
 
 
